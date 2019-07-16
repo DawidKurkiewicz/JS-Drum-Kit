2615bf615842bc657e0d0e2656bf504c1f8d5644
Dawid Kurkiewicz <dawid.kurkiewicz@billennium.com>
2019-07-16 15:45:43 +0200

    added click funcionality

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-window.addEventListener("keydown", function (e){
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-if(!audio) return
-audio.currentTime = 0
-audio.play()
-key.classList.add("playing");
+window.addEventListener("keydown", function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return
+    audio.currentTime = 0
+    audio.play()
+    key.classList.add("playing");
 })
-function removeTransition(e){
-    if(e.propertyName !== "transform") return
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return
     e.target.classList.remove("playing")
 }
 const keys = document.querySelectorAll(".key")
 keys.forEach(key => key.addEventListener("transitionend", removeTransition))
 
+var buttons = document.querySelectorAll("button")
+for(i=0; i<9; i++){
+    buttons.forEach(function (el) {
+        var audio = document.querySelectorAll("audio")[i]
+        el.addEventListener("click", function () {
+            if (audio.dataset.key == el.dataset.key) {
+                audio.currentTime = 0
+                audio.play()
+                el.classList.add("playing")
+            }
+        })
+    })
+}
